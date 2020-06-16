c6dbb9dafa7fffeb81e561f51380bdfac21ffbd5
root <root@N-20HEPF12L0L9.nsn-intra.net>
2020-06-16 18:02:09 +0200

    First commit. String to work on a dataset

---
+++ phd_cand_2020_conf/autorecommender_systems_comparison/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Research results for phd conference 2020',
+    author='Filip Wójcik',
+    license='',
+)
